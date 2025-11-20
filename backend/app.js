13b46deea42f7c33d38234cb6b1f23ba8d51408e
Abdulrhman-Alshafi <Abdulrahman.Alshafi@optimumpartners.com>
2025-11-20 08:56:39 +0300

    adding routes to the app

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,24 @@
-import dotenv from "dotenv";
 import express from "express";
+import dotenv from "dotenv";
 import connectDB from "./config/db";
 import errorHandler from "./middleware/errorHandler";
+import authRoutes from "./routes/authRoutes";
+import blogRoutes from "./routes/blogRoutes";
+import commentRoutes from "./routes/commentRoutes";
+import categoryRoutes from "./routes/categoryRoutes";
+import tagRoutes from "./routes/tagRoutes";
 
 dotenv.config();
 connectDB();
 
 const app = express();
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/blogs", blogRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/categories", categoryRoutes);
+app.use("/api/tags", tagRoutes);
 
 app.use(errorHandler);
 
